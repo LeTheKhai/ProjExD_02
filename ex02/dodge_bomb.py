9473001cd184ca2a1e93b5163143b057643af1c0
The Khai <c0a221537d@edu.teu.ac.jp>
2023-11-28 16:35:13 +0900

    演習1

--- ex02/dodge_bomb.py
+++ ex02/dodge_bomb.py
@@ -3,8 +3,8 @@ import pygame as pg
 import random
 WIDTH, HEIGHT = 1600, 900
 
-delta = { 
-    pg.K_UP: (0, -5), 
+delta = {
+    pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0)
@@ -14,9 +14,9 @@ delta = {
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
 
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:  
+    if rct.left < 0 or WIDTH < rct.right:
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:  
+    if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
 
@@ -27,51 +27,65 @@ def main():
     bg_img = pg.image.load("ex02-20231128/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02-20231128/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()  
-    kk_rct.center = 900, 400 
-    bb_img = pg.Surface((20, 20)) 
-    bb_img.set_colorkey((0, 0, 0))  
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+    bb_img = pg.Surface((20, 20))
+    bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_rct = bb_img.get_rect() 
+    bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, +5  
+    vx, vy = +5, +5
+    delta1 = {
+        (0, -5): pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 90, 1),
+        (0, +5): pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), -90, 1),
+        (+5, 0): pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 0, 1),
+        (-5, 0): pg.transform.rotozoom(kk_img, 0, 1),
+        (-5, -5): pg.transform.rotozoom(kk_img, -45, 1),
+        (-5, +5): pg.transform.rotozoom(kk_img, 45, 1),
+        (+5, -5): pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 45, 1),
+        (+5, +5): pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), -45, 1)
+    }
 
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-        
+
         if kk_rct.colliderect(bb_rct):
             print("Game Over")
             return
-            
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, tpl in delta.items():
             if key_lst[k]:  # キーが押されたら
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-        
 
+        if tuple(sum_mv) in delta1.keys():
+            kk_img = delta1[tuple(sum_mv)]
+            screen.blit(kk_img, kk_rct)
+            pg.display.update()
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)  
-        bb_rct.move_ip(vx, vy) 
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko: 
+        if not yoko:
             vx *= -1
-        if not tate:  
+        if not tate:
             vy *= -1
-        bb_rct.move_ip(vx, vy) 
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 if __name__ == "__main__":
     pg.init()
